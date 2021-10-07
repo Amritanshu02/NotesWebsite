cfc3982f7c6587b2603f595cd065fd9bce205acb
Biohazard <uttu.college.id@gmail.com>
2021-10-07 22:17:33 +0530

    changed notes section and fixed the alert and added styling to it

--- notes.js
+++ notes.js
@@ -29,20 +29,27 @@ function showNotes() {
     notesObj.forEach(function (element, index) {
         if ((element.Title.length > 0 && element.Content.length > 0)) {
             html += `
-            <div class="card my-2 mx-2" style="width: 18rem; background-color: darkgray;">
+            <div class="card my-2 mx-2" style="width: 18rem; background-color: rgba(169, 169, 169, 0.488);">
                 <div class="card-body">
-                    <h5 class="card-title" style="text-decoration:underline;">${element.Title}</h5>
-                    <p class="card-text">${element.Content}</p>
-                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete</button>
+                    <h5 class="card-title note-font" style="text-decoration:underline;">${element.Title}</h5>
+                    <p class="card-text note-font">${element.Content}</p>
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary btn-grad">Delete</button>
                 </div>
                 </div>`;
         }
         else {
             let alert = document.getElementById("alerting");
-            alert.innerHTML = `<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            alert.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+                                <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+                                    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                </symbol>
+                               </svg>
+                               <div class="alert alert-danger d-flex align-items-center" role="alert">
+                                    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+                                    <div>
                                     <strong>Error!</strong> Note heading and content cannot be empty.
-                                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                                    </div>`;
+                                    </div>
+                                </div>`;
             setTimeout(function () {
                 alert.innerHTML = '';
             }, 3000);
