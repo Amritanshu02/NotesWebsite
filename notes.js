8a2ec5636232a860a211073a1d31360bfbe7c6d0
saurav <mesouravofficial@gmail.com>
2021-10-08 23:17:07 +0530

    Update notes.js

--- notes.js
+++ notes.js
@@ -110,3 +110,17 @@ function fadeOut(){
 }
 window.onload = fadeOut;
 // pre loader end
+
+//Twak.to added
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61608300157d100a41ab7897/1fhgfvf98';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
